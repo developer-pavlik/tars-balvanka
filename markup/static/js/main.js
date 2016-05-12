57643ba7872e6c440697fe7ec85f19de58b944a2
Dzekunov Pavel <developer-pavlik@gimil.com>
2016-05-12 16:32:56 +0300

    Балванка

---
+++ markup/static/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*
+    This file can be used as entry point for webpack!
+ */
+
+import $ from 'jquery';
+import Magnific from 'magnific-popup';
+import SlidePanel from 'modules/slide-panel/slide-panel';
+import ProfilPopUp from 'modules/profile-pop-up/profile-pop-up';
+
+
